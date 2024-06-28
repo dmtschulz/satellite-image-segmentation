0bd9907713b7c78c1902c7784cef0b8219994656
dmtschulz <dmtschulz@gmail.com>
2024-06-28 16:02:42 +0200

    simpler unet

--- modeling_tuning/models.py
+++ modeling_tuning/models.py
@@ -28,90 +28,49 @@ class BaselineCNN(nn.Module):
 
 class UNet(nn.Module):
     def __init__(self, in_channels):
-        super().__init__()
+        super(UNet, self).__init__()
 
         # Encoder
-        self.e11 = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
-        self.bn11 = nn.BatchNorm2d(64)
-        self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn12 = nn.BatchNorm2d(64)
+        self.e1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) 
-        self.bn21 = nn.BatchNorm2d(128)
-        self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) 
-        self.bn22 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) 
-    
-        self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) 
-        self.bn31 = nn.BatchNorm2d(256)
-        self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) 
-        self.bn32 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) 
-    
-        self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) 
-        self.bn41 = nn.BatchNorm2d(512)
-        self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) 
-        self.bn42 = nn.BatchNorm2d(512)
+        self.e2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.e3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
 
         # Decoder
-        self.upconv1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.d11 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.bn_d11 = nn.BatchNorm2d(256)
-        self.d12 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.bn_d12 = nn.BatchNorm2d(256)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.d1 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
 
-        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.d21 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.bn_d21 = nn.BatchNorm2d(128)
-        self.d22 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn_d22 = nn.BatchNorm2d(128)
-
-        self.upconv3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.d31 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.bn_d31 = nn.BatchNorm2d(64)
-        self.d32 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn_d32 = nn.BatchNorm2d(64)
+        self.upconv2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.d2 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
 
         # Output layer
-        self.outconv = nn.Conv2d(64, 1, kernel_size=1)
+        self.outconv = nn.Conv2d(32, 1, kernel_size=1)
 
         self.relu = nn.ReLU()
 
     def forward(self, x):
         # Encoder
-        xe11 = self.relu(self.bn11(self.e11(x)))
-        xe12 = self.relu(self.bn12(self.e12(xe11)))
-        xp1 = self.pool1(xe12)
+        xe1 = self.relu(self.e1(x))
+        xp1 = self.pool1(xe1)
 
-        xe21 = self.relu(self.bn21(self.e21(xp1)))
-        xe22 = self.relu(self.bn22(self.e22(xe21)))
-        xp2 = self.pool2(xe22)
+        xe2 = self.relu(self.e2(xp1))
+        xp2 = self.pool2(xe2)
 
-        xe31 = self.relu(self.bn31(self.e31(xp2)))
-        xe32 = self.relu(self.bn32(self.e32(xe31)))
-        xp3 = self.pool3(xe32)
-
-        xe41 = self.relu(self.bn41(self.e41(xp3)))
-        xe42 = self.relu(self.bn42(self.e42(xe41)))
+        xe3 = self.relu(self.e3(xp2))
 
         # Decoder
-        xu1 = self.upconv1(xe42)
-        xu11 = torch.cat([xu1, xe32], dim=1)
-        xd11 = self.relu(self.bn_d11(self.d11(xu11)))
-        xd12 = self.relu(self.bn_d12(self.d12(xd11)))
+        xu1 = self.upconv1(xe3)
+        xu11 = torch.cat([xu1, xe2], dim=1)
+        xd1 = self.relu(self.d1(xu11))
 
-        xu2 = self.upconv2(xd12)
-        xu22 = torch.cat([xu2, xe22], dim=1)
-        xd21 = self.relu(self.bn_d21(self.d21(xu22)))
-        xd22 = self.relu(self.bn_d22(self.d22(xd21)))
-
-        xu3 = self.upconv3(xd22)
-        xu33 = torch.cat([xu3, xe12], dim=1)
-        xd31 = self.relu(self.bn_d31(self.d31(xu33)))
-        xd32 = self.relu(self.bn_d32(self.d32(xd31)))
+        xu2 = self.upconv2(xd1)
+        xu22 = torch.cat([xu2, xe1], dim=1)
+        xd2 = self.relu(self.d2(xu22))
 
         # Output layer
-        out = self.outconv(xd32)
+        out = self.outconv(xd2)
 
-        return out    
+        return out
