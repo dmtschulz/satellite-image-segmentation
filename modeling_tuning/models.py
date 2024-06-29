0e33b27f163cc24db747436cf5d4be4b5302ca4f
dmtschulz <dmtschulz@gmail.com>
2024-06-29 02:40:18 +0200

    0.8466 acc finishing

--- modeling_tuning/models.py
+++ modeling_tuning/models.py
@@ -26,51 +26,123 @@ class BaselineCNN(nn.Module):
         x = self.conv4(x)
         return x
 
+# Adapted from: https://towardsdatascience.com/cook-your-first-u-net-in-pytorch-b3297a844cf3
 class UNet(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, out_channels=1):
         super(UNet, self).__init__()
 
         # Encoder
-        self.e1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
+        self.e1 = nn.Sequential(
+            nn.Conv2d(in_channels, 32, kernel_size=3, padding=1), # 64 too much, it's overfitting already with 32. but yes we can reduce #of layers
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.e2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.e2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.e3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.e3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.e4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Bottleneck
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
         # Decoder
-        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.d1 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.upconv4 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.d4 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
-        self.upconv2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.d2 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
+        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.d3 = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.upconv2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.d2 = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.upconv1 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.d1 = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
         # Output layer
-        self.outconv = nn.Conv2d(32, 1, kernel_size=1)
-
-        self.relu = nn.ReLU()
+        self.outconv = nn.Conv2d(32, out_channels, kernel_size=1)
 
     def forward(self, x):
         # Encoder
-        xe1 = self.relu(self.e1(x))
+        xe1 = self.e1(x)
         xp1 = self.pool1(xe1)
 
-        xe2 = self.relu(self.e2(xp1))
+        xe2 = self.e2(xp1)
         xp2 = self.pool2(xe2)
 
-        xe3 = self.relu(self.e3(xp2))
+        xe3 = self.e3(xp2)
+        xp3 = self.pool3(xe3)
+
+        xe4 = self.e4(xp3)
+        xp4 = self.pool4(xe4)
+
+        # Bottleneck
+        xb = self.bottleneck(xp4)
 
         # Decoder
-        xu1 = self.upconv1(xe3)
-        xu11 = torch.cat([xu1, xe2], dim=1)
-        xd1 = self.relu(self.d1(xu11))
+        xu4 = self.upconv4(xb)
+        xu41 = torch.cat([xu4, xe4], dim=1)
+        xd4 = self.d4(xu41)
 
-        xu2 = self.upconv2(xd1)
-        xu22 = torch.cat([xu2, xe1], dim=1)
-        xd2 = self.relu(self.d2(xu22))
+        xu3 = self.upconv3(xd4)
+        xu31 = torch.cat([xu3, xe3], dim=1)
+        xd3 = self.d3(xu31)
+
+        xu2 = self.upconv2(xd3)
+        xu21 = torch.cat([xu2, xe2], dim=1)
+        xd2 = self.d2(xu21)
+
+        xu1 = self.upconv1(xd2)
+        xu11 = torch.cat([xu1, xe1], dim=1)
+        xd1 = self.d1(xu11)
 
         # Output layer
-        out = self.outconv(xd2)
+        out = self.outconv(xd1)
 
         return out
